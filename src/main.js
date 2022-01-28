0b075e3430ecf3dac4cc374bf4e378846b9bc680
DhruvAnand <ananddhruv29@gmail.com>
2022-01-28 12:37:43 +0530

    Implemented core features

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 const electron = require("electron");
+const { app, BrowserWindow, dialog, ipcMain } = electron;
 
-const { app, BrowserWindow } = electron;
+// fs -file system
+const fs = require('fs');
 
+// declare global app window
+let applicationWindow = null;
 
 // Listen for the app to be ready.
 app.on("ready", () => {
     // Create a BrowserWindow when the app becomes ready.
-    let applicationWindow = new BrowserWindow({
+    applicationWindow = new BrowserWindow({
         show: false,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false 
+            contextIsolation: false ,
+            enableRemoteModule: true
         }
     });
 
@@ -21,16 +26,56 @@ app.on("ready", () => {
 
     if (!app.isPackaged) {
         // Open DevTools if the app is in development (a.k.a not packaged).
-        applicationWindow.webContents.openDevTools();
+    applicationWindow.webContents.openDevTools();
     }
     
     console.log('App Launched Successfully');
 
     // Load the HTML for the page.
     applicationWindow.loadFile('src/index.html');
+
+    applicationWindow.on('closed', () => {
+        applicationWindow = null;
+    });
 });
 
 app.on('window-all-closed', () => {
     if (process.platform !== "darwin") app.quit();
     console.log('Successfully Closed ');
+});
+
+/**
+ * @summary Get the file from user.
+*/
+function getFileFromUser () {
+    dialog.showOpenDialog(applicationWindow, {
+        properties: ['openFile'],
+        filters: [
+            { name: 'Markdown Files', extensions: ['md', 'markdown'] },
+            { name: 'Text Files', extensions: ['txt'] }
+        ]
+    }).then((response) => {
+        if(!response.canceled) {
+            console.log("Opened the file: " + response.filePaths[0]);
+            openFile(response.filePaths[0]);
+        } else {
+            dialog.showErrorBox("Oops, there was an error opening that file", "No file selected.");
+        }
+    });    
+};
+
+/**
+ * Open the file and get its content. (Converted to string)
+*/
+function openFile (file) {
+    console.log("file: " + file);
+    let content = fs.readFile(file, (err, data) => {
+        console.log('The files content is => ' + data.toString());
+        if (err) {dialog.showErrorBox("Oops, there was an error", err)};
+        applicationWindow.webContents.send('file-opened', file, content);
+    });
+};
+
+ipcMain.on('open-file', () => {
+        getFileFromUser();
 });
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 const { marked } = require('marked');
+const { ipcRenderer } = require('electron');
 
-
+// HTML Elements
 const newFileButton = document.querySelector('.new-file-button');
 const openFileButton = document.querySelector('.open-file-btn');
 const saveFileButton = document.querySelector('.save-file-btn');
@@ -16,4 +17,15 @@ const renderMarkdownToHtml = (markdown) => {
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value;
     renderMarkdownToHtml(currentContent);
-})
+});
+
+openFileButton.addEventListener('click', () => {
+    console.log('Open file clicked');
+    ipcRenderer.send('open-file');
+    console.log('Opened the file successfully.');
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+    console.log('Content received = ' + content);
+    console.log('File received = ' + file);
+});
