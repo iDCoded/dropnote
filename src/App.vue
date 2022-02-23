e8f59f8304db8133cc0382c89fe04b35b011b517
DhruvAnand <ananddhruv29@gmail.com>
2022-02-23 23:27:49 +0530

    Added Noto Sans font

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default defineComponent({
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: "Noto Sans", sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
