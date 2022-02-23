34204a3d16d41af6a083e30832f1d6dcafed75f4
DhruvAnand <ananddhruv29@gmail.com>
2022-02-23 22:58:56 +0530

    Added Editor component

---
+++ src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+	<textarea name="markdown" id="md" v-model="editorText"></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "Editor",
+	props: {
+		editorText: String,
+	},
+});
+</script>
