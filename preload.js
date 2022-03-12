ca103a79db29a3ee65e196ddee76c8645aa2d536
Dhruv Anand <ananddhruv29@gmail.com>
2022-03-13 00:08:15 +0530

    Added `on` method for `ipcRenderer`
    Added a method in preload to receive for ipcRenderer.

--- preload.js
+++ preload.js
@@ -10,5 +10,8 @@ const API = {
 	send: (channel, data) => {
 		ipcRenderer.send(channel, data);
 	},
+	on: (channel, data) => {
+		ipcRenderer.on(channel, data);
+	},
 };
 contextBridge.exposeInMainWorld("api", API);
