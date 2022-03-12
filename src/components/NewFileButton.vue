f097b102073bb7e3aa426c8109657e58c893cfd8
Dhruv Anand <ananddhruv29@gmail.com>
2022-03-12 15:27:56 +0530

    Added New File Button
    Added a button to create new file from the welcome screen.

---
+++ src/components/NewFileButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "NewFileButton",
+});
+</script>
+
+<template>
+	<button @click="$emit('new-file')" class="button is-primary new-file-btn">
+		New File
+	</button>
+</template>
--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -1,13 +1,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "./Layout/Header.vue";
+import NewFileButton from "./NewFileButton.vue";
 
 export default defineComponent({
 	name: "WelcomeScreen",
-	components: { Header },
+	components: { Header, NewFileButton },
+	methods: {
+		newFile() {
+			console.log("Created new file");
+		},
+	},
 });
 </script>
 
 <template>
-	<Header appName="Dropnote" />
+	<Header />
+	<NewFileButton v-on:new-file="newFile" />
 </template>
