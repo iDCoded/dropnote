a38efd528aceec0567d58518e5be2ff47235e5e2
Dhruv Anand <ananddhruv29@gmail.com>
2022-03-15 19:12:29 +0530

    Removed unused Header component
    Removed the unused Header component from App as Header is used in WelcomeScreen component.
    Added types for variables.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import Header from "./components/Layout/Header.vue";
 import WelcomeScreen from "./components/WelcomeScreen.vue";
 
-window.api.on("file:opened", (err, content, path) => {
+window.api.on("file:opened", (_err: any, content: any, path: string) => {
 	console.log(content);
 	console.log("----------");
 	console.log(path);
